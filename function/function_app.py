ede2385e4ffed98ea94e322c376e7545ee731354
KeoneShyGuy <KeoneShyGuy@users.noreply.github.com>
2026-06-05 02:11:42 +0000

    Add new tasks for starting the app and development environment; refactor HTTP trigger function to a visit counter

--- function/function_app.py
+++ function/function_app.py
@@ -1,3 +1,5 @@
+from os import name
+
 import azure.functions as func
 import logging
 
@@ -24,23 +26,10 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
 
-@app.route(route="http_trigger", auth_level=func.AuthLevel.FUNCTION)
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="visitCounter")
+def visitCounter(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # This is a simple visit counter function that increments a counter 
+    # stored in Azure Table Storage each time the function is called.
+    return func.HttpResponse("You can count on the counter 👌🏿")
